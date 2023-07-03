1fe14a4227cbba30734d890d352ea6dc61e893f5
Ethan Outangoun <ethanoutangoun@gmail.com>
2023-07-03 10:31:17 -0700

    initial commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,57 @@
+import { useState,useEffect } from 'react'
+import React from 'react'
+import { copy, linkIcon, loader, tick} from '../assets'
+
+
+
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summar: ';'
+  });
+
+
+  const handleSubmit = async (e) => {
+    alert('Submitted');
+  }
+
+  return (
+    <section className='mt-16 w-full max-w-xl'>
+      { /* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form className='relative flex justify-center items-center'
+        onSubmit={handleSubmit}>
+
+          <img
+            src = {linkIcon}
+            alt = "link_icon"
+            className='absolute left-0 my-2 ml-3 w-5'></img>
+
+          <input 
+            type="url"
+            placeholder='Enter a URL'
+            value = ""
+            onChange = {()=> {}}
+            required
+            className='url_input peer'/>
+
+            <button 
+              type = 'submit'
+              className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'
+              >
+                ↲
+            </button>
+
+        </form>
+
+        {/* Browsing History*/}
+
+        
+      </div>
+      {/*Display Results*/}
+    </section>
+  )
+}
+
+export default Demo
